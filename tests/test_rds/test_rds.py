f2da63cc8428c5bfd817ba2b0a54381eee5750dc
Viren Nadkarni <viren.nadkarni@gmail.com>
2025-03-17 19:46:02 +0530

    Revert "RDS: Add `DBCluster.AutoMinorVersionUpgrade` attribute (#8668)"
    This reverts commit 8e122c8f4ec881e1c2e7d232e4877e0fb5d35d71.

--- tests/test_rds/test_rds.py
+++ tests/test_rds/test_rds.py
@@ -61,7 +61,6 @@ def test_create_database(client):
         DBSecurityGroups=["my_sg"],
         VpcSecurityGroupIds=["sg-123456"],
         EnableCloudwatchLogsExports=["audit", "error"],
-        AutoMinorVersionUpgrade=False,
     )
     db_instance = database["DBInstance"]
     assert db_instance["AllocatedStorage"] == 10
@@ -84,7 +83,6 @@ def test_create_database(client):
     assert db_instance["EnabledCloudwatchLogsExports"] == ["audit", "error"]
     assert db_instance["Endpoint"]["Port"] == 1234
     assert db_instance["DbInstancePort"] == 1234
-    assert db_instance["AutoMinorVersionUpgrade"] is False
 
 
 @mock_aws
