ca833032a49e943a589c69a260c083b4aa393dfb
Viren Nadkarni <viren.nadkarni@gmail.com>
2025-03-17 19:32:09 +0530

    Revert "RDS: Migrate to new "serializable" exception base class"
    This reverts commit 0974dd72ef2b13800cbf2bc8d7fdbe1dc61ac4fc.

--- moto/rds/exceptions.py
+++ moto/rds/exceptions.py
@@ -1,9 +1,7 @@
 from typing import Optional
 
-from moto.core.exceptions import ServiceException
 
-
-class RDSClientError(ServiceException):
+class RDSClientError(Exception):
     def __init__(self, code: str, message: str):
         super().__init__(message)
         self.code = code
@@ -47,7 +45,7 @@ class DBSubnetGroupNotFoundError(RDSClientError):
 class DBParameterGroupNotFoundError(RDSClientError):
     def __init__(self, db_parameter_group_name: str):
         super().__init__(
-            "DBParameterGroupNotFoundFault",
+            "DBParameterGroupNotFound",
             f"DB Parameter Group {db_parameter_group_name} not found.",
         )
 
